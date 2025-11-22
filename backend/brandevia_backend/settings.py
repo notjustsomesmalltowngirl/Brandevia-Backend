4d45fa1807d5d2fade8072c59e9484a5dee24006
notjustsomesmalltowngirl <olukayodepeace2006@gmail.com>
2025-11-22 11:51:42 +0100

    update databases for hosting

--- backend/brandevia_backend/settings.py
+++ backend/brandevia_backend/settings.py
@@ -120,22 +120,18 @@ WSGI_APPLICATION = 'brandevia_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default='sqlite:///db.sqlite3',
-#     )
-# }
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.getenv("DATABASE_URL")
-#     )
-# }
 
 
 # Password validation
