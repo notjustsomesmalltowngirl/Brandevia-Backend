6e6614183a1850d313f7914ed38380c34366fc30
notjustsomesmalltowngirl <olukayodepeace2006@gmail.com>
2025-11-22 12:53:08 +0100

    add route for creating a superuser

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CustomTokenObtainPairView
+from .views import CustomTokenObtainPairView, create_temp_superuser
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("make-admin/", create_temp_superuser),
+
 ]
