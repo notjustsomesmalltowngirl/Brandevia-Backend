cc3c9a36d55b85c6e135cf464bc7435a7b33e54a
notjustsomesmalltowngirl <olukayodepeace2006@gmail.com>
2025-11-22 12:42:07 +0100

    add route for creating a superuser

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,5 +1,25 @@
 from .serializers import CustomTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def create_temp_superuser(request):
+    User = get_user_model()
+
+    if User.objects.filter(email="admin@gmail.com").exists():
+        return Response({"status": "exists"})
+
+    User.objects.create_superuser(
+        email="admin@gmail.com",
+        password="123admin123",
+        username='admin'
+    )
+
+    return Response({"status": "created"})
